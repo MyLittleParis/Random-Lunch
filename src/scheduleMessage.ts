c6e6435a3d91484221fbe3cac6ccf8eac22776ab
Nakaoni <cho_tenro@hotmail.fr>
2022-10-29 00:01:44 +0200

    POC: prepare a schedule lunch message on start

---
+++ src/scheduleMessage.ts
@@ -0,0 +1,24 @@
+import { Methods } from "@slack/web-api";
+import { DateTime } from "luxon";
+
+export async function scheduleLunchMessage(chat: Methods["chat"]) {
+
+  const response = await chat.scheduledMessages.list();
+
+  if (response.scheduled_messages?.length !== 0) {
+    return;
+  }
+
+  const date = DateTime
+    .now()
+    .setZone("Europe/Paris")
+    .startOf("day")
+    .plus({ days: 1, hours: 11, minutes: 30 })
+    .toUnixInteger();
+
+  await chat.scheduleMessage({
+    channel: process.env.CHANNEL_ID as string,
+    post_at: date,
+    text: "What do we eat for lunch today ? :thinking_face:"
+  });
+}
